eb438797738d8fefc1e4453fb96ea8fb09b4c905
Kira <yuna-@web.de>
2021-08-03 03:13:48 +0000

    Updated dependencies
    Former-commit-id: 5d81270653157956f60109223c911fd458bf5fe8

--- src/main/java/berlin/yuna/natsserver/config/NatsSourceConfig.java
+++ src/main/java/berlin/yuna/natsserver/config/NatsSourceConfig.java
@@ -13,7 +13,7 @@ public enum NatsSourceConfig {
     DEFAULT(LINUX.defaultValue, "[STRING] DEFAULT SOURCE URL"),
     ;
 
-    private static final String DEFAULT_VERSION = "v2.3.2";
+    private static final String DEFAULT_VERSION = "v2.3.3";
 
     private final String defaultValue;
     private final String description;
